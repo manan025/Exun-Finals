4391b9729fab815c46cd92353c0f6c8324d08235
Jai Nanda <jainanda1411@gmail.com>
2022-11-12 23:51:34 +0530

    wallet

--- web/src/Pages/Wallet.js
+++ web/src/Pages/Wallet.js
@@ -1,28 +1,90 @@
 import Sidebar from '../components/Sidebar'
 import '../Styles/Wallet.css'
-
+import kanye from '../static/kanye.jpg'
+import kanye2 from '../static/kanye2.png'
 const Wallet = () => {
   return (
     <>
-    <div className="container flexbox">
-        <Sidebar/>
+    <div className="grid">
+
+        <div className="column">
     <div className="card">
         <h1>My Wallet</h1>
         <div className="credit">
             <h3>Total Balance</h3>
             <h2>$69,422.00 <span>USD</span></h2>
-            <p>November 14 ,2022 <span>2.42% increase</span></p>
+            <p>November 14 ,2022 <span>2.42% ^</span></p>
         </div>
     </div>
     <div className="transactions">
         <h1>Recent Transactions <span>see all</span></h1>
         <div className="money">
-            <div className="img"><img src="" alt="" /></div>
             <div className="account">
-                <h2>Kanye West</h2>
+            <img src={kanye} alt="" />
+            <div className="bhai">
+            <h2>Kanye West</h2>
+                <p>Heartless</p>        
+            </div>
+        
             </div>
             <div className="balance">
                 <h4>+$33</h4>
+            </div>
+        </div>
+        <div className="money">
+            <div className="account">
+            <img src={kanye} alt="" />
+            <div className="bhai">
+            <h2>Kanye West</h2>
+                <p>Heartless</p>        
+            </div>
+        
+            </div>
+            <div className="balance">
+                <h4>+$33</h4>
+            </div>
+        </div>
+        <div className="money">
+            <div className="account">
+            <img src={kanye} alt="" />
+            <div className="bhai">
+            <h2>Kanye West</h2>
+                <p>Heartless</p>        
+            </div>
+        
+            </div>
+            <div className="balance">
+                <h4>+$33</h4>
+            </div>
+        </div>
+        
+    </div>
+    </div>
+    <div className="column2">
+        <div className="profile">
+            <h1>My Profile</h1>
+            <div className="kanyeG">
+                <div className="img">
+                    <img src={kanye2} alt="" />
+                </div>
+                <div className="info">
+                    <p><span>Kanye West</span></p>
+                    <p><span>Email</span>    kanyeG@jenner.com</p>
+                    <p><span>Office</span>    N**as in Paris</p>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <h1>Card Actions</h1>
+            <div className="action">
+                <div className="content">
+                    <div className="icons">
+
+                    </div>
+                    <div className="text">
+                        <p>Tranfer</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
